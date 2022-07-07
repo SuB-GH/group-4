899c035a899a4fd3bf08f93f4e09c16b6decfcf2
Venus DM <crhowe90@gmail.com>
2022-07-07 10:41:58 -0500

    get weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,18 @@ var citySearchEl = document.querySelector("#city-search");
 var cityNameEl = document.querySelector("#city-name");
 var cityNameHistory = [];
 var cityName 
+
+//get the weather for a city
+var getWeatherData = function(newLat, newLon, city){
+var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + newLat + "&lon=" + newLon + "&units=imperial&appid=386d421121bbbad42dc1ad82319e7fc0";
+fetch(apiUrl).then(function(response){
+    if(response.ok){
+        response.json().then(function (data){
+            console.log(data);
+        });
+    }
+});
+};
 // find Lat / Lon from city input
 var getCity = function (city) {
     // insert API url
