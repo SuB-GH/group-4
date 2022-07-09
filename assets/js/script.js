51ad679d399bed59de1f2b787d3004fe03ff4001
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-07-09 12:12:48 -0500

    added iss location api with predictive passes. user inputs city, iss api uses lat & lon from input search

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,21 @@
 var citySearchEl = document.querySelector("#city-search");
 var cityNameEl = document.querySelector("#city-name");
 var cityNameHistory = [];
-var cityName 
+var cityName
 
 //get the weather for a city
-var getWeatherData = function(newLat, newLon, city){
-var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + newLat + "&lon=" + newLon + "&units=imperial&appid=386d421121bbbad42dc1ad82319e7fc0";
-fetch(apiUrl).then(function(response){
-    if(response.ok){
-        response.json().then(function (data){
-            console.log(data);
-        });
-    }
-});
+var getWeatherData = function (issLat, issLon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + issLat + "&lon=" + issLon + "&units=imperial&appid=386d421121bbbad42dc1ad82319e7fc0";
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+                console.log(data.daily[0].clouds, "Clouds");
+                console.log(data.daily[0].rain, "Rain");
+                console.log(data.daily[0].weather[0].description, "Description");
+            });
+        }
+    });
 };
 // find Lat / Lon from city input
 var getCity = function (city) {
@@ -24,18 +27,38 @@ var getCity = function (city) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
-                
+
                 // Lat & Lon variables:
                 newLat = data[0].lat;
                 newLon = data[0].lon;
-                getWeatherData (newLat, newLon);
-                // may need city also?
+                getISS(newLat, newLon);
+                getWeatherData(newLat, newLon);
+            })
+        }
+    })
+};
+
+// API call for ISS position
+var getISS = function (newLat, newLon) {
+    //             // API url
+    var issRequestUrl = "https://api.g7vrd.co.uk/v1/satellite-passes/25544/" + newLat + "/" + newLon + ".json";
+
+    fetch(issRequestUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+                console.log(data.lat, "Lat");
+                console.log(data.lon, "Lon");
+                issLat = data.latitude;
+                issLon = data.longitude;
             })
         }
     })
 };
 
 
+
+// getWeatherData();
 // Form submission
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -43,14 +66,14 @@ var formSubmitHandler = function (event) {
     // get value from input element
     var cityName = cityNameEl.value.trim();
     // cityNameHistory.push(cityName);
-if (cityName) {
+    if (cityName) {
 
 
-    // call getCity function
-getCity(cityName);
+        // call getCity function
+        getCity(cityName);
 
-    cityNameEl.value = "";
-}
+        cityNameEl.value = "";
+    }
 };
 
 // Event listener
