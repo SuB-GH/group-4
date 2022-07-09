77f474399b58feea66f4f577b6b9e3b1653d57eb
Venus DM <crhowe90@gmail.com>
2022-07-09 12:56:11 -0500

    merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,15 @@ var cityNameHistory = [];
 var cityName
 
 //get the weather for a city
-var getWeatherData = function (newLat, newLon, city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + newLat + "&lon=" + newLon + "&units=imperial&appid=386d421121bbbad42dc1ad82319e7fc0";
+var getWeatherData = function (issLat, issLon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + issLat + "&lon=" + issLon + "&units=imperial&appid=386d421121bbbad42dc1ad82319e7fc0";
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                //console.log(data);
+                console.log(data);
+                console.log(data.daily[0].clouds, "Clouds");
+                console.log(data.daily[0].rain, "Rain");
+                console.log(data.daily[0].weather[0].description, "Description");
             });
         }
     });
@@ -23,19 +26,39 @@ var getCity = function (city) {
     fetch(requestUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                //console.log(data);
+                console.log(data);
 
                 // Lat & Lon variables:
-                var newLat = data[0].lat;
-                var newLon = data[0].lon;
+                newLat = data[0].lat;
+                newLon = data[0].lon;
+                getISS(newLat, newLon);
                 getWeatherData(newLat, newLon);
-                // may need city also?
+            })
+        }
+    })
+};
+
+// API call for ISS position
+var getISS = function (newLat, newLon) {
+    //             // API url
+    var issRequestUrl = "https://api.g7vrd.co.uk/v1/satellite-passes/25544/" + newLat + "/" + newLon + ".json";
+
+    fetch(issRequestUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+                console.log(data.lat, "Lat");
+                console.log(data.lon, "Lon");
+                issLat = data.latitude;
+                issLon = data.longitude;
             })
         }
     })
 };
 
 
+
+// getWeatherData();
 // Form submission
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -44,8 +67,11 @@ var formSubmitHandler = function (event) {
     var cityName = cityNameEl.value.trim();
     // cityNameHistory.push(cityName);
     if (cityName) {
+
+
         // call getCity function
         getCity(cityName);
+
         cityNameEl.value = "";
     }
 };
